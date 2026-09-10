1057084f52b3e95f27f625a6d9a1a03adb3288a1
Gabriel Maroneze Ramos <gabimaronezeramos@hotmail.com>
2026-09-10 10:58:12 -0300

    classe ponto de coleta e arquivos front/bdd

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
+
+class PontoColeta(BaseModel):
+    nome: str
+    endereco: str
+    latidude: float
+    longitude: float
+    endereco: str
+    materiais: Enum[str]
+    horario_abertura: str
+    horario_fechamento: str
+    numero_contato: int
+    descricao: str | None = None    
 
 @app.get("/")
 def inicio():
     return {"mensagem": "Minha API está funcionando"}
+
