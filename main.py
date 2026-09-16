e0361e2f3c5afa4632324b35350194a5df7d5479
Gabriel Maroneze Ramos <gabimaronezeramos@hotmail.com>
2026-09-16 17:34:48 -0300

    Correcao da integracao da main com o postgres

--- main.py
+++ main.py
@@ -1,36 +1,63 @@
 from fastapi import FastAPI
-from database import conectar 
+from database import conectar
 
 app = FastAPI()
-
 
 @app.get("/")
 def inicio():
     return {"mensagem": "Minha API está funcionando"}
+
+
 @app.get("/pontos")
 def listar_pontos():
     """
-    Retorna os pontos existentes na tabela pontos_coleta.
+    Retorna os pontos de coleta e seus respectivos materiais.
     """
-    
+
     with conectar() as conexao:
         with conexao.cursor() as cursor:
             cursor.execute(
                 """
                 SELECT
-                    id,
-                    nome,
-                    endereco,
-                    latitude,
-                    longitude,
-                    horario_abertura,
-                    horario_fechamento,
-                    numero_contato,
-                    descricao
-                FROM pontos_coleta
-                ORDER BY id;
+                    p.id,
+                    p.nome,
+                    p.endereco, 
+                    p.latitude,
+                    p.longitude,
+                    p.horario_abertura,
+                    p.horario_fechamento,
+                    p.numero_contato,
+                    p.descricao,
+
+                COALESCE(
+                    ARRAY_AGG(m.nome ORDER BY m.nome)
+                        FILTER (WHERE m.id IS NOT NULL),
+                    ARRAY[]::VARCHAR[]
+                    ) AS materiais
+
+                FROM public.pontos_coleta AS p
+
+                LEFT JOIN public.ponto_material AS pm
+                    ON pm.ponto_id = p.id
+
+                LEFT JOIN public.materiais AS m
+                    ON m.id = pm.material_id
+
+                GROUP BY
+                    p.id,
+                    p.nome,
+                    p.endereco,
+                    p.latitude,
+                    p.longitude,
+                    p.horario_abertura,
+                    p.horario_fechamento,
+                    p.numero_contato,
+                    p.descricao
+
+                ORDER BY p.id;
                 """
             )
-            
+
             pontos = cursor.fetchall()
+
     return pontos
